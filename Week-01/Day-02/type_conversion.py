3dd82ad1a37c44c87f876d15fb28ed2d443539a3
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-27 09:45:24 +0500

    Create type_conversion.py

---
+++ Week-01/Day-02/type_conversion.py
@@ -0,0 +1,86 @@
+### converting string into integer
+
+##code:
+
+```python
+
+x = int(input("Enter first number: "))
+
+y = int(input("Enter second number: "))
+
+print(x + y)
+
+```
+## input :
+
+```
+10
+20
+
+```
+
+##output:
+
+```
+30
+
+```
+#pros:
+- to tell system that given number is an integer 
+#cons:
+- system will treat numbers as string and then the output will be not satisfying.
+
+
+### converting string into integer
+
+##code:
+
+``` python 
+
+height = float(input("Enter your height: "))
+print(height)
+
+```
+
+##input:
+
+```
+5.7
+
+```
+
+#output:
+
+```
+5.7
+
+```
+#pros:
+- tells system that given number is a decimal number .
+#cons:
+- treat decimal number as a string or text .
+
+
+### converting numbers into strings
+
+## code :
+
+```python
+
+age = 18
+
+print("Age: " + str(age))
+
+```
+
+##output:
+
+```
+Age: 18
+
+```
+##purpose :
+- converts number into text 
+- pyhton gives error without str() .
+  
+
