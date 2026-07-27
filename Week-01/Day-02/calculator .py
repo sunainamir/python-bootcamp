a2ca3fb701f80c02149d951013cf2955d0c39d03
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-27 09:56:10 +0500

    Create calculator .py

---
+++ Week-01/Day-02/calculator .py
@@ -0,0 +1,43 @@
+# my first calculator
+
+##code:
+
+```python 
+
+# Simple Calculator
+
+first = int(input("Enter first number: "))
+
+second = int(input("Enter second number: "))
+
+print("Addition:", first + second)
+
+print("Subtraction:", first - second)
+
+print("Multiplication:", first * second)
+
+print("Division:", first / second)
+
+```
+
+##input:
+
+```
+Enter first number : 2
+Enter second number: 2
+
+##output:
+
+```
+Addition: 4
+Subtraction: 0
+Multiplication: 4
+Division: 2.0
+
+```
+
+### features: 
+
+- calculate two numbers
+- perform operations ( + , - , * , / )
+- satisfying results
