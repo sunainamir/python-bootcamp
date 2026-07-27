b7ea986569c251a86824529dadcb7826692f57e7
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-27 17:12:27 +0500

    Update exercises.py

--- Week-01/Day-02/exercises.py
+++ Week-01/Day-02/exercises.py
@@ -56,5 +56,110 @@ you are  18 years old
 ## description:
 - new technique to return the value of a variable that is f-string.
 
+### EXERCISE 3
+
+Ask the user for:
+
+Mathematics marks
+Physics marks
+
+Print the total.
+
+##code:
+
+```python
+
+maths = int(input("enter marks of mathematics : "))
+phy = int(input("enter marks of physics : "))
+total = maths + phy
+print(f"your total marks of both subjects = {total}")
+
+```
+
+##input :
+
+```
+enter marks of mathematics : 95
+enter marks of physics : 92
+
+```
+
+## output:
+
+```
+your total marks of both subjects = 187
+
+```
+
+## description :
+- usage of int()
+- arithmetic operations
+
+
+### EXERCISE 4
+
+Ask for height using float().
+
+##code:
+
+```python
+
+height = float(input("enter your height : "))
+
+print(f"your height is  {heigth}")
+
+```
+
+##input :
+
+```
+enter your height : 6.7
+
+```
+
+## output:
+
+```
+your height is 6.7
+
+```
+
+## description :
+- usage of float()
+
+
+### EXERCISE 5
+
+Print a decorative title .
+
+
+##code:
+
+```python
+
+print("=" * 20)
+print ("   student report  ")
+print("=" * 20)
+  
+
+```
+
+
+## output:
+
+```
+====================
+  student report  
+====================
+  
+```
+
+## description :
+- usage of "=" * 20
+- leads to decorative heading.
+
+
+
+
 
 
