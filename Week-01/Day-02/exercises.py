04b0500a6bbeecaa300a38a80069acef96dc1e7e
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-27 10:14:38 +0500

    Create exercises.py

---
+++ Week-01/Day-02/exercises.py
@@ -0,0 +1,60 @@
+</> Markdown
+
+### EXERCISE 1 
+
+Write a program that prints: "Welcome to Python Programming" 
+using comment.
+
+##code:
+
+```python
+# welcome string
+print( "Welcome to Python Programming" )
+
+```
+
+##output:
+
+```
+Welcome to Python Programming
+
+```
+
+### EXERCISE 2
+
+Ask the user for their name and age and print them .
+Use an f-string. 
+
+##code:
+
+```python
+
+name = input("enter your name : ")
+age = int(input("enter your age : ")
+
+print(f"hello! {name}")
+print(f("you are {age} years old ")
+
+```
+
+##input :
+
+```
+enter your name : sunaina
+enter your age : 18
+
+```
+
+## output:
+
+```
+hello! sunaina
+you are  18 years old 
+
+```
+
+## description:
+- new technique to return the value of a variable that is f-string.
+
+
+
