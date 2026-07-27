58789987ba0c239dadd4eb6014881be87f540fc1
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-27 09:30:18 +0500

    Update comment.py

--- Week-01/Day-02/comment.py
+++ Week-01/Day-02/comment.py
@@ -1,2 +1,18 @@
-#code:
+## my first comment 
+##code:
+
+```python
+# this program prints my name
+print("sunaina")
+
+```
+
+#output:
+```
+sunaina
+
+```
+
+#description :
+ writting such programs that is easy to understand 
 
