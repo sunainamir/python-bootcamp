6b1b617c66bca0c1d0efe8c0a4578440d5db9371
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-27 07:31:17 +0500

    Create mini_project.py

---
+++ Week-01/Day-01/mini_project.py
@@ -0,0 +1,41 @@
+## mini project
+
+Student Profile Program
+
+### Features
+
+- Takes user input
+- Displays a formatted profile
+
+###code:
+
+```python
+
+name=input("what is your name ?") 
+age=input("what is your age? ")
+country=input("what is the name of your country? ")
+favsub=input("what is your favourite subject ")  
+print("  -----student profile-----")   
+print("name : ",name)    print("\n  age:",age)   
+print("\n  country:",country) print("\n  favourite subject :",favsub)  
+print("thankyou!")
+
+```
+###output:
+
+```
+----- Student Profile -----
+
+Name : Sunaina
+
+Age : 18
+
+Country : Pakistan
+
+Favorite Subject : Physics
+
+Thank you!
+
+```
+
+ 
