a85e60e6769e1398bab9f41597a86eddfdae8562
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-28 12:12:39 +0500

    Create comparision_operators.py

---
+++ Week-01/Day-03/comparision_operators.py
@@ -0,0 +1,117 @@
+#comparision_operators
+
+- operators used to compare two values .
+
+## Equal to operator (==)
+  
+  - python uses this operator to compare two values equally. 
+
+  ###sample code
+  
+   ```python
+  
+       print(7==7)
+
+   ```
+
+  ###sample output :
+
+  ```
+       True
+ 
+  ```
+
+## not equal to operator (!=)
+  
+  - pyhton uses this operator when two values are unequal 
+  
+  ###sample code
+  
+   ```python
+  
+        print(7!=7)
+
+   ```
+
+  ###sample output :
+
+    ```
+         False
+
+    ```
+## less than operator (<)
+  
+  - pyhton compares both values and prints the lesser one .
+  
+  ###sample code
+  
+   ```python
+  
+        print(15<20)
+
+   ```
+
+  ###sample output :
+
+    ```
+         True
+
+    ```
+## greater than operator (>)
+  
+  - pyhton compares both values and prints the greater one .
+  
+  ###sample code
+  
+   ```python
+  
+        print(15>20)
+
+   ```
+
+  ###sample output :
+
+    ```
+         False
+
+    ```
+## greater than or equal to operator (>=)
+  
+  - pyhton compares both values and prints the one which is greater or equal to other value.
+  
+  ###sample code
+  
+   ```python
+  
+        print(20>=20)
+
+   ```
+
+  ###sample output :
+
+    ```
+         True
+
+    ```
+## less than or equal to operator (<=)
+  
+  - pyhton compares both values and prints the one which is less than or equal to the other value .
+  
+  ###sample code
+  
+   ```python
+  
+        print(15<=20)
+
+   ```
+
+  ###sample output :
+
+    ```
+         True
+
+    ```
+
+# what I learned
+  - how to use comparision operators .
+  - how these operators makes our decision easier. 
