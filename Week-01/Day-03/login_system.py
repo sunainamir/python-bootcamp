daaf14852a6e2b909b4e5dd62215f73dbd990a92
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-29 05:55:52 +0500

    Create login_system.py

---
+++ Week-01/Day-03/login_system.py
@@ -0,0 +1,110 @@
+# login_system
+
+##features
+- secure because username and password both are required 
+- better visual 
+- uses logical operators 
+
+### code 
+
+```python
+name =  input("Enter username :")
+password = input("Enter password :")
+
+if name == "sunaina" and password =='python123' :
+    print("="*25)
+    print("    AI system login")
+    print("="*25)
+    print("\nlogin successful ! ")
+    print("\nWelcome sunaina ")
+    print("\nHave a great day ")
+    print("="*25)
+    
+elif name == "sunaina" and password !='python123' :
+    print("Access denied ! ")
+    print("wrong password. try again! ")
+    
+elif name != "sunaina" and password =='python123' :
+    print("access denied ! ")
+    print("wrong username . try again! ")
+    
+elif name != "sunaina" and password !='python123' :
+    print("invalid data  ! ")
+    print("please try again !")
+
+
+```
+
+### sample input 1
+
+```
+Enter username :sunaina
+Enter password :python123
+
+```
+
+### sample output 1
+
+```
+=========================
+    AI system login
+=========================
+
+login successful ! 
+
+Welcome sunaina 
+
+Have a great day 
+=========================
+  
+```
+
+### sample input 2
+
+```
+Enter username :sunaina
+Enter password :abc123
+
+```
+
+### sample output 2 
+
+```
+Access denied ! 
+wrong password. try again! 
+
+```
+
+### sample input 3
+
+```
+Enter username :maisam
+Enter password :python123
+
+```
+
+### sample output 3
+
+```
+Access denied ! 
+wrong username . try again! 
+
+```
+
+### sample input 4
+
+```
+Enter username :maisam
+Enter password :abc123
+
+```
+
+### sample output 4
+
+```
+
+invalid data  ! 
+please try again !
+
+```
+
