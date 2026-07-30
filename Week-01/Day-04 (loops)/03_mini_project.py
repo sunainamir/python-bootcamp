33d449e55d15c3e29966cda725a0ac01a58eeb4e
Sunaina Mir <sunainamir.dev@gmail.com>
2026-07-30 21:33:39 +0500

    Create 03_mini_project.py

---
+++ Week-01/Day-04 (loops)/03_mini_project.py
@@ -0,0 +1,19 @@
+"""
+==========================================
+Mini Project: Number Analyzer
+
+Description:
+This project demonstrates the use of Python loops,
+conditions, number processing, data summation,
+and pattern printing.
+
+Concepts Used:
+- for loop
+- range()
+- if statements
+- arithmetic operators
+- data summation
+- pattern printing
+==========================================
+"""
+
