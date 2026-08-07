2e6137e7c853ef4356d003e5cdf25f6d710f85b2
Sunaina Mir <sunainamir.dev@gmail.com>
2026-08-07 10:08:48 +0500

    update day 4

--- Week-02/Day-04-Advance-Functions/03_mini_project.py
+++ Week-02/Day-04-Advance-Functions/03_mini_project.py
@@ -11,11 +11,70 @@ theme = input("Enter Theme : ")
 mic = input("Enable Microphone (True/False) : ")
 speaker = input("Enable Speaker (True/False) : ")
 auto_save = input("Enable Auto Save (True/False) : ")   
+print()
 add_languages = int(input("How many favorite languages do you want to add? "))
+print()
 
-def get_languages():
+def languages():
     for i in range(add_languages):
-        input(f"Enter language {i} : " )
-        return get_languages
+        input(f"Enter Language {i+1} : ")
+        
+languages()
+
+print() 
+print("="*40)
+print("AI SETTINGS".center(40))
+print("="*40)
+print()
+
+def welcome():
+    print(f"Welcome {name}")
+welcome()
+print()
+print("Application name : BridgeTalk AI")
+print()
+
+def settings(**details):
     
-get_languages()
+    for key , value in details.items():
+        print(key ," : ",value)
+        
+settings(Language = pre_language ,
+         Voice = voice ,
+         Speed=  speed ,
+         Theme = theme ,
+         Microphone = mic ,
+         Speaker = speaker ,
+         Auto_Save = auto_save )
+
+print()
+print("="*40)
+print("FAVOURITE LANGAUGES".center(40))
+print("="*40)
+print() 
+
+#def fav_languages(*langauges):
+#   print()
+
+print()
+print("="*40)
+print("LOCAL VARIABLE DEMO".center(40))
+print("="*40)
+print()      
+print("Current Mode : Translation")
+
+print()
+print("="*40)
+print("GLOBAL VARIABLE DEMO".center(40))
+print("="*40)
+print() 
+print("Application Name : BridgeTalk AI")
+
+print()
+print("="*40)
+print("SETTINGS SAVED SUCCESSFULLY".center(40))
+print("="*40)
+print() 
+
+print("Thank You For Using BridgeTalk AI Setting Manager !")
+print()
